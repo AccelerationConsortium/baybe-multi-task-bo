a5f765791116efb78559a6f5276360d5da265192
dpersaud <hithammer523@hotmail.com>
2024-07-14 23:34:31 -0400

    WIP - fixed error in 03 so now the counts are correct (previously would over count for columns / elements removed in a previous step) and nearly completed the multi-task work - things are failing for an unknown reason

--- persaudD/03_systemIdentification.py
+++ persaudD/03_systemIdentification.py
@@ -9,13 +9,12 @@
 
 #%%
 # IMPORT DEPENDENCIES------------------------------------------------------------------------------
-import config
 import os
 import logging
 import sys
-from datetime import datetime
 import pandas as pd
 import itertools
+import time
 
 # SET UP LOGGING-------------------------------------------------------------------------------
 
@@ -105,6 +104,9 @@ only appear in one dataframe are not useful for the analysis and can be removed 
 number of checks in the subsequent steps.
 '''
 
+# drop the ElementFraction Exceptions columns
+dfMP.drop(columns="ElementFraction Exceptions", inplace=True)
+dfExp.drop(columns="ElementFraction Exceptions", inplace=True)
 
 # find the common columns between the two dataframes
 lstCommonElementCols = list(set(lstNonZeroCols_mp).intersection(lstNonZeroCols_exp))
@@ -113,14 +115,20 @@ lstCommonElementCols = list(set(lstNonZeroCols_mp).intersection(lstNonZeroCols_e
 lstNonCommonElementCols_mp = [col for col in lstNonZeroCols_mp if col not in lstCommonElementCols]
 lstNonCommonElementCols_exp = [col for col in lstNonZeroCols_exp if col not in lstCommonElementCols]
 
+# for entries in dfMP with non-zero values in the non-common columns, drop them
+dfMP = dfMP[~dfMP[lstNonCommonElementCols_mp].any(axis=1)]
+# for entries in dfExp with non-zero values in the non-common columns, drop them
+dfExp = dfExp[~dfExp[lstNonCommonElementCols_exp].any(axis=1)]
+'''
+Entries in the dataframes that have non-zero values in the non-common columns cannot overlap
+with any entries in the other dataframe because the non-common columns are not present in the
+other dataframe. Therefore, we can remove these entries to reduce the number of checks in the
+subsequent steps.
+'''
+
 # drop the columns that are not in the common columns
 dfMP.drop(columns=lstNonCommonElementCols_mp, inplace=True)
 dfExp.drop(columns=lstNonCommonElementCols_exp, inplace=True)
-
-# drop the ElementFraction Exceptions columns
-dfMP.drop(columns="ElementFraction Exceptions", inplace=True)
-dfExp.drop(columns="ElementFraction Exceptions", inplace=True)
-
 
 
 #%%
@@ -130,83 +138,114 @@ we want to count the number of entries which have at least one non-zero value fo
 combination and zero values for all other elements.
 '''
 
-intSystemSize = 6
+# intSystemSize = 3
+
+def getCombinationCount (intSystemSize, lstCommonElementCols):
+    # generate all combinations of elements from the common columns
+    lstElementCombinations = list(itertools.combinations(lstCommonElementCols, intSystemSize))
+
+    # initialize a dictionary to store the count of entries for each combination
+    dicCombinationCounts = {}
+
+    # iterate over each combination
+    for tupCombination_temp in lstElementCombinations:
+        # get the list of other elements
+        lstOtherElements_temp = [col for col in lstCommonElementCols if col not in tupCombination_temp]
+        
+        # select the columns for the current combination
+        dfMP_system_temp = dfMP[list(tupCombination_temp)]
+        dfExp_system_temp = dfExp[list(tupCombination_temp)]
+
+        # check for rows where at least one value in the combination is non-zero
+        srContainsCombination_mp = dfMP_system_temp.any(axis=1)
+        srContainsCombination_exp = dfExp_system_temp.any(axis=1)
+
+        # check for rows where all other elements have zero values
+        srOnlyContainsCombination_mp = (dfMP[lstOtherElements_temp] == 0).all(axis=1)
+        srOnlyContainsCombination_exp = (dfExp[lstOtherElements_temp] == 0).all(axis=1)
+
+        # count the number of rows satisfying both conditions
+        srValidRows_mp = srContainsCombination_mp & srOnlyContainsCombination_mp
+        srValidRows_exp = srContainsCombination_exp & srOnlyContainsCombination_exp
+
+        # count the number of valid rows
+        intCount_mp = srValidRows_mp.sum()
+        intCount_exp = srValidRows_exp.sum()
+
+        # find the indices of the valid rows
+        lstIndices_mp = srValidRows_mp[srValidRows_mp].index.tolist()
+        lstIndices_exp = srValidRows_exp[srValidRows_exp].index.tolist()
+
+        # extract the entries from the tuple and make them a string
+        strCombination_temp = ",".join(tupCombination_temp)
 
 
-# generate all combinations of elements from the common columns
-lstElementCombinations = list(itertools.combinations(lstCommonElementCols, intSystemSize))
+        # store the count of valid rows in the dictionary
+        dicCombinationCounts[strCombination_temp] = {
+            "count_mp": intCount_mp,
+            "count_exp": intCount_exp,
+            "indices_mp": lstIndices_mp,
+            "indices_exp": lstIndices_exp
+        }
 
-# initialize a dictionary to store the count of entries for each combination
-dicCombinationCounts = {}
+    # convert the dictionary to a dataframe
+    dfCombinationCounts = pd.DataFrame.from_dict(dicCombinationCounts).T
+    # save the dataframe to a csv file
+    dfCombinationCounts.to_csv(os.path.join(strWD, "data", f"combinationCounts_{intSystemSize}.csv"))
+    logging.info(f"Saved the combination counts to {os.path.join(strWD, 'data', f'combinationCounts_{intSystemSize}.csv')}")
 
-# iterate over each combination
-for tupCombination_temp in lstElementCombinations:
-    # get the list of other elements
-    lstOtherElements_temp = [col for col in lstCommonElementCols if col not in tupCombination_temp]
+    intMaxCount_exp = 0 
+    intMaxCount_mp = 0
+
+    # for every row in the dataframe, find the number of valid rows
+    for idx, row in dfCombinationCounts.iterrows():
+        intCount_mp = row["count_mp"]
+        intCount_exp = row["count_exp"]
+        if intCount_mp > intMaxCount_mp and intCount_exp > intMaxCount_exp:
+            intMaxCount_mp = intCount_mp
+            intMaxCount_exp = intCount_exp
+            strBestCombination = idx
+
+    return dfCombinationCounts, strBestCombination, intMaxCount_mp, intMaxCount_exp
+
+
+#%%
+# start the timer
+timeStart = time.time()
+print("Starting the timer for combinations of 2...")
+dfCombinationCounts_2, strBestCombination_2, intMaxCount_mp_2, intMaxCount_exp_2 = getCombinationCount(2, lstCommonElementCols)
+timeEnd = time.time()
+print(f"Finished in {timeEnd - timeStart} seconds\n")
+
+# start the timer
+timeStart = time.time()
+print("Starting the timer for combinations of 3...")
+dfCombinationCounts_3, strBestCombination_3, intMaxCount_mp_3, intMaxCount_exp_3 = getCombinationCount(3, lstCommonElementCols)
+timeEnd = time.time()
+print(f"Finished in {timeEnd - timeStart} seconds\n")
+
+# start the timer
+timeStart = time.time()
+print("Starting the timer for combinations of 4...")
+dfCombinationCounts_4, strBestCombination_4, intMaxCount_mp_4, intMaxCount_exp_4 = getCombinationCount(4, lstCommonElementCols)
+timeEnd = time.time()
+print(f"Finished in {timeEnd - timeStart} seconds\n")
+
+# # start the timer
+# timeStart = time.time()
+# print("Starting the timer for combinations of 5...")
+# dfCombinationCounts_5, strBestCombination_5, intMaxCount_mp_5, intMaxCount_exp_5 = getCombinationCount(5, lstCommonElementCols)
+# timeEnd = time.time()
+# print(f"Finished in {timeEnd - timeStart} seconds\n")
+
+# #%%
+# # start the timer
+# timeStart = time.time()
+# print("Starting the timer for combinations of 6...")
+# dfCombinationCounts_6, strBestCombination_6, intMaxCount_mp_6, intMaxCount_exp_6 = getCombinationCount(6, lstCommonElementCols)
+# timeEnd = time.time()
+# print(f"Finished in {timeEnd - timeStart} seconds\n")
+
+
     
-    # select the columns for the current combination
-    dfMP_system_temp = dfMP[list(tupCombination_temp)]
-    dfExp_system_temp = dfExp[list(tupCombination_temp)]
-
-    # check for rows where at least one value in the combination is non-zero
-    srContainsCombination_mp = dfMP_system_temp.any(axis=1)
-    srContainsCombination_exp = dfExp_system_temp.any(axis=1)
-
-    # check for rows where all other elements have zero values
-    srOnlyContainsCombination_mp = (dfMP[lstOtherElements_temp] == 0).all(axis=1)
-    srOnlyContainsCombination_exp = (dfExp[lstOtherElements_temp] == 0).all(axis=1)
-
-    # count the number of rows satisfying both conditions
-    srValidRows_mp = srContainsCombination_mp & srOnlyContainsCombination_mp
-    srValidRows_exp = srContainsCombination_exp & srOnlyContainsCombination_exp
-
-    # count the number of valid rows
-    intCount_mp = srValidRows_mp.sum()
-    intCount_exp = srValidRows_exp.sum()
-
-    # find the indices of the valid rows
-    lstIndices_mp = srValidRows_mp[srValidRows_mp].index.tolist()
-    lstIndices_exp = srValidRows_exp[srValidRows_exp].index.tolist()
-
-    # extract the entries from the tuple and make them a string
-    strCombination_temp = ",".join(tupCombination_temp)
-
-
-    # store the count of valid rows in the dictionary
-    dicCombinationCounts[strCombination_temp] = {
-        "count_mp": intCount_mp,
-        "count_exp": intCount_exp,
-        "indices_mp": lstIndices_mp,
-        "indices_exp": lstIndices_exp
-    }
-
-#%%
-
-# find the combination with the highest number of valid rows
-intMaxCount_mp = 0
-intMaxCount_exp = 0
-tupBestCombination = None
-for tupCombination_temp, dicCounts_temp in dicCombinationCounts.items():
-    if dicCounts_temp["count_mp"] > intMaxCount_mp and dicCounts_temp["count_exp"] > intMaxCount_exp:
-        intMaxCount_mp = dicCounts_temp["count_mp"]
-        intMaxCount_exp = dicCounts_temp["count_exp"]
-        tupBestCombination = tupCombination_temp
-
-#%%
-# SAVE THE RESULTS-------------------------------------------------------------------------------
-'''
-save the dictionary of combination counts to a csv file
-'''
-
-# create a dataframe from the dictionary
-dfCombinationCounts = pd.DataFrame.from_dict(dicCombinationCounts).T
-
-# save the dataframe to a csv file
-strCombinationCountsFilePath = os.path.join(strWD, "data", f"combinationCounts_{intSystemSize}.csv")
-
-dfCombinationCounts.to_csv(strCombinationCountsFilePath)
-logging.info(f"Saved the combination counts to {strCombinationCountsFilePath}")
-
-
-# dicCombinationCounts_6 = dfCombinationCounts
 # %%
